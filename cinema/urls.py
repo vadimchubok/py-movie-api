33ad3ae570bae44e05b1305504734ec5624da801
Vadim <vadimchubok@gmail.com>
2025-12-28 20:32:41 +0200

    Fix to fix

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,6 +3,8 @@ from .views import MovieListCreateView, MovieRetrieveUpdateDeleteView
 
 
 urlpatterns = [
-    path('movies/', MovieListCreateView.as_view(), name='movie-list-create'),
-    path('movies/<int:pk>/', MovieRetrieveUpdateDeleteView.as_view(), name='movie-detail'),
+    path("movies/", MovieListCreateView.as_view(), name="movie-list-create"),
+    path(
+        "movies/<int:pk>/", MovieRetrieveUpdateDeleteView.as_view(), name="movie-detail"
+    ),
 ]
--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/cinema/', include('cinema.urls')),
+    path("admin/", admin.site.urls),
+    path("api/cinema/", include("cinema.urls")),
 ]
