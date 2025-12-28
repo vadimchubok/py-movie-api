4b42809184bf693d315b30a2e97246b5cc28853e
Vadim <vadimchubok@gmail.com>
2025-12-28 20:26:55 +0200

    Fix

--- cinema/urls.py
+++ cinema/urls.py
@@ -3,6 +3,6 @@ from .views import MovieListCreateView, MovieRetrieveUpdateDeleteView
 
 
 urlpatterns = [
-    path("movies/", MovieListCreateView.as_view(), name="movie-list-create"),
-    path("movies/<int:pk>/", MovieRetrieveUpdateDeleteView.as_view(), name="movie-detail"),
+    path('movies/', MovieListCreateView.as_view(), name='movie-list-create'),
+    path('movies/<int:pk>/', MovieRetrieveUpdateDeleteView.as_view(), name='movie-detail'),
 ]
